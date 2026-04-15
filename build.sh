a039fb378ec4bdc0220cc00701da2a4b42c9a95a
8ig1s0 <codlet.gentoo@proton.me>
2026-04-15 22:40:23 +0700

    Update build.sh (oops)

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 # Placeholder for build script
 # Build dependencies: SDL2, GLEW, glm, compatible OpenGL drivers, iostream and cmath
+# I will move to CMake eventually when the project isn't just 3 files
 clang++ main.cpp -o cube -lSDL2 -lGLEW -lGL -ldl
